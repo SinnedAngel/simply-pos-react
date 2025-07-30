cf523abd4c6e4e47f61b8f72b49a4f9a7b7285d9
Arman Hendra Harnanda <sinned.angel@gmail.com>
2025-07-30 13:40:41 +0700

    Update config.ts

--- data/config.ts
+++ data/config.ts
@@ -2,6 +2,6 @@
 // This file is the central place for all external service configurations.
 
 export const SUPABASE_CONFIG = {
-  URL: 'https://cgmiycshdkbrspecfuvn.supabase.co', // Replace with your Supabase project URL
-  ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNnbWl5Y3NoZGticnNwZWNmdXZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0Mjc3OTYsImV4cCI6MjA2OTAwMzc5Nn0.fPmNaiVnTLcJ7VJBUmslrHaINw2bgB1-TnhUi7k_LPk', // Replace with your Supabase anon key
+  URL: '', // Replace with your Supabase project URL
+  ANON_KEY: '', // Replace with your Supabase anon key
 };
